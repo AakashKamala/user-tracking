52a68214e7e8a9a966d8c004495dfa119150aaa1
Aakash <aakashkumar2908@gmail.com>
2025-01-01 23:53:12 +0530

    update

--- client/src/components/MediaTrack.jsx
+++ client/src/components/MediaTrack.jsx
@@ -5,29 +5,29 @@ const MediaTrack = () => {
   const [stream, setStream]=useState(null)
   const videoRef = useRef(null);
 
-  // useEffect(() => {
-  //   const media = async () => {
-  //     try {
-  //       const streamReceived = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-  //       console.log('Media stream received:', streamReceived);
-  //       setStream(streamReceived)
-  //       if (videoRef.current) {
-  //         videoRef.current.srcObject = streamReceived;
-  //       }
-  //     } catch (error) {
-  //       console.error('Error accessing media devices:', error);
-  //     }
-  //   };
+  useEffect(() => {
+    const media = async () => {
+      try {
+        const streamReceived = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        console.log('Media stream received:', streamReceived);
+        setStream(streamReceived)
+        if (videoRef.current) {
+          videoRef.current.srcObject = streamReceived;
+        }
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
+      }
+    };
 
-  //   media();
-  // }, []);
+    media();
+  }, []);
 
 
 
 
   return (<div>
     <h1>MediaTrack</h1>
-    {/* <video ref={videoRef} width="500" height="300" autoPlay controls ></video> */}
+    <video ref={videoRef} width="500" height="300" autoPlay controls ></video>
   </div>);
 };
 
