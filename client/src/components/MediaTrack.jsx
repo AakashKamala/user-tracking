915d8379f7699e415e3c6e79fb928400e2a597b4
Aakash <aakashkumar2908@gmail.com>
2025-01-02 23:53:40 +0530

    screen access

--- client/src/components/MediaTrack.jsx
+++ client/src/components/MediaTrack.jsx
@@ -4,6 +4,8 @@ const MediaTrack = () => {
 
   const [stream, setStream]=useState(null)
   const videoRef = useRef(null);
+  const [screen, setScreen]=useState(null)
+  const screenRef = useRef(null);
 
   useEffect(() => {
     const media = async () => {
@@ -23,11 +25,30 @@ const MediaTrack = () => {
   }, []);
 
 
+  useEffect(() => {
+    const screen = async () => {
+      try {
+        const screenReceived = await navigator.mediaDevices.getDisplayMedia();
+        console.log('Screen stream received:', streamReceived);
+        setScreen(screenReceived)
+        if (screenRef.current) {
+          screenRef.current.srcObject = screenReceived;
+        }
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
+      }
+    };
+
+    screen();
+  }, []);
+
+
 
 
   return (<div>
     <h1>MediaTrack</h1>
     <video ref={videoRef} width="500" height="300" autoPlay controls ></video>
+    <video ref={screenRef} width="500" height="300" autoPlay controls ></video>
   </div>);
 };
 
