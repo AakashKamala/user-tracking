0d5ba3e020e1d1b4064d6a63f9ee552966bcb572
Aakash <aakashkumar2908@gmail.com>
2025-01-05 19:55:51 +0530

    ip controller update1

--- server/controllers/ip.js
+++ server/controllers/ip.js
@@ -1,12 +1,30 @@
-const { default: axios } = require("axios")
+// const { default: axios } = require("axios")
 
-const getIP=async(req, res)=>{
-    console.log(req.ip)
-    const ip = req.ip
-    const gp="24.48.0.1"
-    const details=await axios(`http://ip-api.com/json/${gp}`)
-    console.log(details.data)
-    return res.json({"ip": ip})
-}
+// const getIP=async(req, res)=>{
+//     console.log(req.ip)
+//     const ip = req.ip
+//     const gp="24.48.0.1"
+//     const details=await axios(`http://ip-api.com/json/${gp}`)
+//     console.log(details.data)
+//     return res.json({"ip": ip})
+// }
 
-module.exports=getIP
+// module.exports=getIP
+
+
+
+const { default: axios } = require("axios");
+
+const getIP = async (req, res) => {
+    // Enable trust proxy in app config to get accurate IP in production
+    const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
+    console.log("Client IP:", ip);
+
+    const gp = "24.48.0.1"; // Example IP for geo-location test
+    const details = await axios(`http://ip-api.com/json/${gp}`);
+    console.log("Geo Details:", details.data);
+
+    return res.json({ ip, geoDetails: details.data });
+};
+
+module.exports = getIP;
