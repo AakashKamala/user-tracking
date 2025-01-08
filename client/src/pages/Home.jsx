e3312ac5cab4e37f0cad03c3577aa311e3799fd3
Aakash <aakashkumar2908@gmail.com>
2025-01-08 23:05:24 +0530

    some landing page update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+        <h1>A cybersecurity platform</h1>
+        <p>with all the advancements in tech, cybersecurity has become a critical field.</p>
+        <p>people need to know every way they can be tracked on the internet</p>
+        <p>it can be scrolling social media, google searches, or visiting any platform</p>
+        <p>if your device has any sort of connection, there is a chance, you can be monitored, or tracked</p>
+        <br />
+        <h2>This platform will try to inform you abi=out the ways you can be tracked/monitored.</h2>
+        <p>explore the patform inside out to get to know the ways platforms can implement to track you</p>
+    </div>
   )
 }
 
